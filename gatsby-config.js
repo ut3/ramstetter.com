e8edeb509e734ed6bc59d90353937b622b1aee23
J Rick Ramstetter <rick@anteaterllc.com>
2020-04-07 16:35:06 -0700

    Add google analytics tracking code UA-163107742-1
    Signed-off-by: J Rick Ramstetter <rick@anteaterllc.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: '',
+        trackingId: 'UA-163107742-1',
       },
     },
     {
