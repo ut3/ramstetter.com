2f21446ed7276d40114c6f97b0dd100ca54daae9
J Rick Ramstetter <rick@anteaterllc.com>
2020-04-07 22:50:20 -0700

    Make navi wrap.
    Signed-off-by: J Rick Ramstetter <rick@anteaterllc.com>

--- src/components/navi/index.js
+++ src/components/navi/index.js
@@ -6,13 +6,13 @@ class Navi extends React.Component {
   render() {
     const { location, title } = this.props
     return (
-      <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary">
-        <div className="container">
+      <nav className="navbar navbar-expand navbar-dark flex-column flex-lg-row bg-primary">
+        <div className="container flex-wrap">
           <Link className="text-center" to="/">
             <h1 className="navbar-brand mb-0">{title}</h1>
           </Link>
           <div className="navbar-nav-scroll">
-            <ul className="navbar-nav bd-navbar-nav flex-row">
+            <ul className="navbar-nav bd-navbar-nav">
               <li
                 className={
                   location.pathname === '/profile/'
