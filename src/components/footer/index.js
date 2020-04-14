99d89616a0b3fb7097d6f3e891d1e7f3492ee0ee
J Rick Ramstetter <rick@anteaterllc.com>
2020-04-14 10:48:59 -0700

    Fix asterisks in comments
    Signed-off-by: J Rick Ramstetter <rick@anteaterllc.com>

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * This project incorporates work covered by the following copyright and
  * permission notices
  * 
