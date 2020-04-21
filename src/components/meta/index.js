74254ec20247e12e454ba2d9fda92fe3c8e86200
J Rick Ramstetter <rick@anteaterllc.com>
2020-04-21 16:46:30 -0700

    Make meta a class
    Signed-off-by: J Rick Ramstetter <rick@anteaterllc.com>

--- src/components/meta/index.js
+++ src/components/meta/index.js
@@ -2,31 +2,34 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import get from 'lodash/get'
 
-const Meta = ({ site, title, location }) => {
-  const siteTitle = get(site, 'title')
-  title = title ? `${title} | ${siteTitle}` : siteTitle
-  return (
-    <Helmet
-      title={title}
-      meta={[
-        { property: 'og:title', content: title },
-        { property: 'og:locale', content: 'en_US' },
-        { property: 'og:type', content: 'website' },
-        {
-          property: 'og:description',
-          content: get(site, 'description'),
-        },
-        {
-          property: 'og:url',
-          content: `${get(site, 'siteUrl')}/`,
-        },
-        {
-          property: 'og:image',
-          content: `${get(site, 'siteUrl')}/rick-ramstetter.jpg`,
-        },
-      ]}
-    />
-  )
+class Meta extends React.Component {
+  render() {
+    const { site, title, location } = this.props
+    const siteTitle = get(site, 'title')
+    title = title ? `${title} | ${siteTitle}` : siteTitle
+    return (
+      <Helmet
+        title={title}
+        meta={[
+          { property: 'og:title', content: title },
+          { property: 'og:locale', content: 'en_US' },
+          { property: 'og:type', content: 'website' },
+          {
+            property: 'og:description',
+            content: get(site, 'description'),
+          },
+          {
+            property: 'og:url',
+            content: `${get(site, 'siteUrl')}/`,
+          },
+          {
+            property: 'og:image',
+            content: `${get(site, 'siteUrl')}/rick-ramstetter.jpg`,
+          },
+        ]}
+      />
+    )
+  }
 }
 export default Meta
 
