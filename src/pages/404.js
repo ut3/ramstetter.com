43f9aae15780c59f802e0b882108331316dcfb2f
J Rick Ramstetter <rick@anteaterllc.com>
2020-05-02 10:54:33 -0700

    Add 404 page
    Signed-off-by: J Rick Ramstetter <rick@anteaterllc.com>

---
+++ src/pages/404.js
@@ -0,0 +1,72 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Copyright 2020 J Rick Ramstetter, rick.ramstetter@gmail.com
+ *
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This project incorporates work covered by the following copyright and
+ * permission notices
+ *
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 gatsbyjs
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+import React from 'react'
+import Link from 'gatsby-link'
+
+import Layout from 'components/layout'
+import Meta from 'components/meta'
+import ProfileBlurb from 'components/profileblurb'
+
+class Contact extends React.Component {
+  render() {
+    const { location, data } = this.props
+
+    return (
+      <Layout location={location}>
+        <Meta title="Not Found" location={location} />
+        <div>
+          <div className="container">
+            <ProfileBlurb />
+            <h1>Resource not found</h1>
+            <p>
+              The content you requested was not found. You've followed a broken
+              link or entered a URL that doesn't exist on this site.
+            </p>
+            <p>
+              <Link to="/">Main page</Link>
+            </p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Contact
